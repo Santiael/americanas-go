4a78ebaa2d480535c43deeb98bd5ea86396d4535
Rafael Santiago <rafaelsantiagods@gmail.com>
2020-05-03 16:58:03 -0300

    💩  Fix first time access dummy tab

--- src/navigation/DummyTab.js
+++ src/navigation/DummyTab.js
@@ -2,11 +2,11 @@ import { useEffect } from 'react';
 
 export default function DummyTab({ navigation }) {
   useEffect(() => {
-    const unsubscribe = navigation.addListener('tabPress', (e) => {
+    navigation.addListener('tabPress', (e) => {
       e.preventDefault();
     });
 
-    return unsubscribe;
+    navigation.goBack();
   }, [navigation]);
 
   return null;
